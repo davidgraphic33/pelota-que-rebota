76b92a1920e38cb50c9ab0221ad2c0321dbaafa7
David Bravo <userd.lu78@gmail.com>
2025-11-06 11:56:04 -0500

    se creo un sistema de pelotas

--- sketch.js
+++ sketch.js
@@ -1,37 +1,23 @@
-let posx;
-let posy;
-let diam;
-let rad;
+const NP = 100;
+let pelotas = [];
 
-let velx;
-let vely;
-
-function setup() {
+function setup(){
     createCanvas(windowWidth, windowHeight);
-    diam = random(50, 300);
-    rad = diam / 2;
+    rectMode(CENTER);
 
-    posx = random(rad, width - rad);
-    posy = random(rad, height - rad);
-
-    velx = random(-5, 5);
-    vely = random(-5, 5);
+    for(let i = 0; i < NP; i++){
+        let nuevaPelota = new Pelota();
+        pelotas[i] = nuevaPelota;
+    }
+    
 }
 
-    function draw() {
-        background(150);
+function draw() {
+    background(150);
 
-        if(posx > width - rad || posx < rad){
-            velx *= -1;
-        }
-
-        if(posy > width - rad || posy < rad){
-            vely *= -1;
-        }
-
-        posx += velx;
-        posy += vely;
-
-        circle( posx, posy, diam);
+    for(let i = 0; i < NP; i++){
+        pelotas[i].actualizar();
+        pelotas[i].visualizar();
+    }
 
 }
